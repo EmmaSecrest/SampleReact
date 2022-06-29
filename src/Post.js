a7e9a7f72dfcad5f748168273f73262fee19d7e7
Emma Secrest <esecrest@dminc.com>
2022-06-29 17:00:16 -0400

    showing body of selected post

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export function Post() {
   const [allBlogPost, setBlogPost] = useState([]);
+  const [selectedPost, setSelectedPost] = useState([]);
 
   async function fetchAllPosts() {
     const post = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -15,13 +16,25 @@ export function Post() {
     })();
   }, []);
 
-  return allBlogPost.map((post, index) => (
-    <div key={index}>
-      <h3>{post.title}</h3>
-      <p>{post.body}</p>
-      <p>{post.userId}</p>
-      <p>{post.id}</p>
+  function handlePostClick(e) {
+    const postId = Number(e.target.id);
+    const clickedPost = allBlogPost.filter((post) => postId === post.id);
+    console.log(clickedPost[0]);
+    // console.log(clickedPost);
+    setSelectedPost(clickedPost[0]);
+  }
+
+  return (
+    <div>
+      {allBlogPost.map((post, index) => (
+        <div key={index}>
+          <h3 onClick={handlePostClick} id={post.id}>
+            {post.title}
+          </h3>
+        </div>
+      ))}
+      <p>{selectedPost.body}</p>
     </div>
-  ));
+  );
 }
 export default Post;
